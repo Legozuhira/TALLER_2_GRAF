3c2db05b4373adbdc0c07b7133dc3eede1c2e5e2
Legozuhira <79022858+Legozuhira@users.noreply.github.com>
2021-11-30 19:17:47 -0500

    Add files via upload
    taller #2

---
+++ components/Tutorial.vue
@@ -0,0 +1,289 @@
+
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-200 sm:items-center sm:pt-0"
+  >
+    <link
+      href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css"
+      rel="stylesheet"
+    />
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <a
+        class="flex justify-center pt-8 sm:pt-0"
+        href="https://nuxtjs.org"
+        target="_blank"
+      >
+        <svg class="h-16" width="296" height="87" viewBox="0 0 296 87" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M102 27.476h8.755l18.556 29.6674V27.476h8.082v42.0374h-8.696l-18.615-29.6058v29.6058H102V27.476ZM173.519 69.5132h-7.531v-4.6956c-1.715 3.3045-5.204 5.1651-9.612 5.1651-6.981 0-11.514-4.9245-11.514-12.0119V39.2441h7.532v17.1772c0 4.1438 2.573 7.0258 6.43 7.0258 4.225 0 7.164-3.243 7.164-7.6862V39.2441h7.531v30.2691ZM199.729 69.0347l-7.226-10.0897-7.227 10.0897h-8.022L188.4 53.2398l-10.287-14.4742h8.204l6.186 8.7104 6.122-8.7104h8.267l-10.35 14.4742 11.146 15.7949h-7.959ZM222.997 30.1165v9.127h8.572V45.43h-8.572v15.0259c0 .5047.204.9887.567 1.3458.364.3571.857.5581 1.372.5589h6.633v7.1402h-4.961c-6.735 0-11.143-3.8445-11.143-10.5651V45.4359h-6.062v-6.1924h3.803c1.774 0 2.807-1.0359 2.807-2.7498v-6.3772h6.984ZM250.556 59.993V27.476h8.079v30.0254c0 8.1087-3.98 12.012-10.715 12.012h-9.405v-7.1403h9.614c.644 0 1.261-.2508 1.716-.6971.455-.4464.711-1.0517.711-1.683ZM263.477 55.3996h7.78c.431 4.6956 3.614 7.6861 8.758 7.6861 4.593 0 7.78-1.799 7.78-5.5232 0-9.0097-23.04-2.7645-23.04-18.8587C264.76 31.7983 270.823 27 279.533 27c9.064 0 15.186 5.1035 15.56 12.9716h-7.738c-.365-3.6626-3.291-6.1248-7.837-6.1248-4.225 0-6.858 1.9222-6.858 4.8629 0 9.309 23.34 2.3478 23.34 18.7384C296 65.2545 289.447 70 280.018 70c-9.737 0-16.104-5.7668-16.532-14.5945" fill="#012A35"/><path fill-rule="evenodd" clip-rule="evenodd" d="M39.115 19.0524c-2.2607-3.8871-7.9124-3.8871-10.1731 0L4.30396 61.4154c-2.26069 3.8871.56516 8.7459 5.08654 8.7459h19.2337c-1.9321-1.6885-2.6475-4.6094-1.1855-7.1155l18.6598-31.9859-6.9835-12.0075Z" fill="#80EEC0"/><path d="M54.726 28.3449c1.8709-3.1803 6.5482-3.1803 8.4191 0l20.39 34.6606c1.871 3.1804-.4677 7.1558-4.2095 7.1558H38.5455c-3.7418 0-6.0805-3.9754-4.2095-7.1558l20.39-34.6606Z" fill="#00DC82"/></svg>
+      </a>
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6 ">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Taller #2
+        </h2>
+        <p class="mt-3 text-gray-600">
+          Esta pagina esta echa completamente en
+          <a
+            href="https://nuxtjs.org"
+            target="_blank"
+            class="text-green-500 hover:underline"
+            >Nuxt</a
+          >.<br />
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+          Paso a Paso de lo aprendido en clase
+  
+        </p>
+      </div>
+      
+
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6 ">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Instalar node js
+        </h2>
+        <p class="mt-3 text-gray-600">
+          Node.js® es un entorno de ejecución para JavaScript.
+          <br />
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+          Ir a
+          <code class="bg-gray-100 text-sm p-1 rounded border"
+            >https://nodejs.org/es/</code
+          >
+           y instala la versión lts de acuerdo al sistema opeativo.
+          
+        </p>
+
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Usar create-nuxt-app
+        </h2>
+         <p class="mt-3 text-gray-600">
+          create-nuxt-app Crea la estructura para comensar a progrmar sin preocumarnos de eso
+          <br />
+        </p>
+          
+          
+        </p>
+      </div>
+
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6 ">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Crer archivo de formulario 
+        </h2>
+        <p class="mt-3 text-gray-600">
+          Crear archivo llamado Formualrio.vue con el siguiente codigo
+          <br />
+        </p>
+
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+        
+          <pre  class="bg-gray-100 text-sm p-1 rounded border">
+    &lt;template&gt;
+      &lt;div class="container"&gt;
+        &lt;h1 class="title"&gt;
+          Contacto
+        &lt;/h1&gt;
+        &lt;div class="content"&gt;
+          &lt;form name="contact" action="" method="post"&gt;
+            &lt;label class="form-label" for="name"&gt;
+              Nombre :
+            &lt;/label&gt;
+            &lt;input class="form-field" name="name" id="name" /&gt;
+            &lt;label class="form-label" for="email"&gt;
+              Apellido :
+            &lt;/label&gt;
+            &lt;input class="form-field" name="email" id="email" /&gt;
+            &lt;label class="form-label" for="message"&gt;
+              Correo:
+            &lt;/label&gt;
+            &lt;textarea class="form-field" name="message" id="message"&gt; &lt;/textarea&gt;
+            &lt;input class="form-button" type="submit" value="Send message" /&gt;
+          &lt;/form&gt;
+        &lt;/div&gt;
+      &lt;/div&gt;
+    &lt;/template&gt;
+        </pre> 
+        </p>
+
+      </div>
+
+       <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6 ">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Formulario
+        </h2>
+        <p class="mt-3 text-gray-600">
+          El formulario se debe de ver asi
+          .<br />
+        </p>
+        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
+         <form
+      v-on:submit.prevent="sendMessage"
+    >
+            <div class="form">
+      <div class="title">Taller #2</div>
+      <div class="subtitle">Contacto</div>
+      <div class="input-container ic1">
+        <input v-model="nombre" id="firstname" class="input" type="text" placeholder=" " />
+        <div class="cut"></div>
+        <label for="firstname" class="placeholder">Nombre</label>
+      </div>
+      <div class="input-container ic2">
+        <input v-model="apellido" id="lastname" class="input" type="text" placeholder=" " />
+        <div class="cut"></div>
+        <label for="lastname" class="placeholder">Apellido</label>
+      </div>
+      <div class="input-container ic2">
+        <input v-model="correo" id="email" class="input" type="text" placeholder=" " />
+        <div class="cut cut-short"></div>
+        <label for="email" class="placeholder">Correo</label>
+      </div>
+      <button type="text" class="submit">Subir</button>
+    </div>
+    </form>
+        </p>
+      </div>
+    </div>
+  </div>
+
+  
+</template>
+
+
+
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: "",
+      apellido: "",
+      correo: "",
+    };
+  },
+  methods: {
+    sendMessage() {
+      alert("Nombre: " + this.nombre + ".\n Apellido: "+ this.apellido + ".\n"
+      +"Correo: "+ this.correo + ".\n")
+    },
+  }
+};
+</script>
+
+<style>
+
+.form {
+  background-color: #00DC82;
+  border-radius: 20px;
+  box-sizing: border-box;
+  height: 500px;
+  padding: 20px;
+  width: 100%;
+}
+
+.title {
+  color: #eee;
+  font-family: sans-serif;
+  font-size: 36px;
+  font-weight: 600;
+  margin-top: 30px;
+}
+
+.subtitle {
+  color: #eee;
+  font-family: sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  margin-top: 10px;
+}
+
+.input-container {
+  height: 50px;
+  position: relative;
+  width: 100%;
+}
+
+.ic1 {
+  margin-top: 40px;
+}
+
+.ic2 {
+  margin-top: 30px;
+}
+
+.input {
+  background-color: #303245;
+  border-radius: 12px;
+  border: 0;
+  box-sizing: border-box;
+  color: #eee;
+  font-size: 18px;
+  height: 100%;
+  outline: 0;
+  padding: 4px 20px 0;
+  width: 100%;
+}
+
+.cut {
+  background-color: #00DC82;
+  border-radius: 10px;
+  height: 20px;
+  left: 20px;
+  position: absolute;
+  top: -20px;
+  transform: translateY(0);
+  transition: transform 200ms;
+  width: 76px;
+}
+
+.cut-short {
+  width: 50px;
+}
+
+.input:focus ~ .cut,
+.input:not(:placeholder-shown) ~ .cut {
+  transform: translateY(8px);
+}
+
+.placeholder {
+  color: #65657b;
+  font-family: sans-serif;
+  left: 20px;
+  line-height: 14px;
+  pointer-events: none;
+  position: absolute;
+  transform-origin: 0 50%;
+  transition: transform 200ms, color 200ms;
+  top: 20px;
+}
+
+.input:focus ~ .placeholder,
+.input:not(:placeholder-shown) ~ .placeholder {
+  transform: translateY(-30px) translateX(10px) scale(0.75);
+}
+
+.input:not(:placeholder-shown) ~ .placeholder {
+  color: #808097;
+}
+
+.input:focus ~ .placeholder {
+  color: #dc2f55;
+}
+
+.submit {
+  background-color: #012A35;
+  border-radius: 12px;
+  border: 0;
+  box-sizing: border-box;
+  color: #eee;
+  cursor: pointer;
+  font-size: 18px;
+  height: 50px;
+  margin-top: 38px;
+  outline: 0;
+  text-align: center;
+  width: 100%;
+}
+
+.submit:active {
+  background-color: #06b;
+}
+</style>
